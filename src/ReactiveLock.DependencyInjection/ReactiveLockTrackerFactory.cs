b57915191af1181952bc0ee93315dd76f0e89cb1
Michel Oliveira <michel.nyx@hotmail.com>
2025-07-25 15:54:32 -0300

    refactor: simplify service provider access in ReactiveLockTrackerFactory

--- src/ReactiveLock.DependencyInjection/ReactiveLockTrackerFactory.cs
+++ src/ReactiveLock.DependencyInjection/ReactiveLockTrackerFactory.cs
@@ -12,17 +12,17 @@ public interface IReactiveLockTrackerFactory
 
 public class ReactiveLockTrackerFactory : IReactiveLockTrackerFactory
 {
-    private readonly IServiceProvider _serviceProvider;
+    private IServiceProvider ServiceProvider { get; }
 
     public ReactiveLockTrackerFactory(IServiceProvider serviceProvider)
     {
-        _serviceProvider = serviceProvider;
+        ServiceProvider = serviceProvider;
     }
 
     public IReactiveLockTrackerState GetTrackerState(string lockKey)
     {
         var stateKey = ReactiveLockConventions.GetStateKey(lockKey);
-        var keyedProvider = _serviceProvider.GetKeyedService<IReactiveLockTrackerState>(stateKey)
+        var keyedProvider = ServiceProvider.GetKeyedService<IReactiveLockTrackerState>(stateKey)
             ?? throw new InvalidOperationException($"No state keyed service provider available for key '{stateKey}'.");
         return keyedProvider;
     }
@@ -30,7 +30,7 @@ public class ReactiveLockTrackerFactory : IReactiveLockTrackerFactory
     public IReactiveLockTrackerController GetTrackerController(string lockKey)
     {
         var controllerKey = ReactiveLockConventions.GetControllerKey(lockKey);
-        var keyedProvider = _serviceProvider.GetKeyedService<IReactiveLockTrackerController>(controllerKey)
+        var keyedProvider = ServiceProvider.GetKeyedService<IReactiveLockTrackerController>(controllerKey)
             ?? throw new InvalidOperationException($"No controller keyed service provider available for key '{controllerKey}'.");
         return keyedProvider;
     }
