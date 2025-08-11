527828d6da5b0a5b479a3759791d438b15d590d1
Michel Oliveira <michel.nyx@hotmail.com>
2025-08-11 18:24:16 -0300

    add ReactiveLock.Distributed.Grpc

---
+++ src/ReactiveLock.Distributed.Grpc/ReactiveLockGrpcTrackerStore.cs
@@ -0,0 +1,47 @@
+namespace MichelOliveira.Com.ReactiveLock.Distributed.Grpc;
+
+using global::ReactiveLock.Distributed.Grpc;
+using static global::ReactiveLock.Distributed.Grpc.ReactiveLockGrpc;
+
+using MichelOliveira.Com.ReactiveLock.Core;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class ReactiveLockGrpcTrackerStore(ReactiveLockGrpcClient client, string lockKey, string instanceName)
+    : IReactiveLockTrackerStore
+{
+    public static (bool allIdle, string? lockData) AreAllIdleFromRpc(LockStatusNotification update)
+    {
+        if (update.InstancesStatus.Count == 0)
+            return (true, null);
+
+        var busyEntries = update.InstancesStatus
+            .Where(kv => kv.Value.IsBusy)
+            .Select(kv => kv.Value.LockData)
+            .ToArray();
+
+        if (busyEntries.Length == 0)
+            return (true, null);
+
+        var extraParts = busyEntries
+            .Where(extra => !string.IsNullOrEmpty(extra))
+            .ToArray();
+
+        string? lockData = extraParts.Length > 0
+            ? string.Join(IReactiveLockTrackerState.LOCK_DATA_SEPARATOR, extraParts)
+            : null;
+
+        return (false, lockData);
+    }
+
+    public async Task SetStatusAsync(string _, bool isBusy, string? lockData = null)
+    {
+        await client.SetStatusAsync(new LockStatusRequest
+        {
+            LockKey = lockKey,
+            InstanceId = instanceName,
+            IsBusy = isBusy,
+            LockData = lockData
+        }).ConfigureAwait(false);
+    }
+}
